21dd82666f70aeabee98004cb05db26120766632
Michael Bestas <mkbestas@lineageos.org>
2020-12-30 21:58:08 +0200

    config: Add dataservices to PRODUCT_SOONG_NAMESPACES if needed
    The mk files in vendor/qcom/opensource/dataservices were
    converted to bp, so this is needed to avoid duplicate module
    build errors on devices that use a different dataservices lib.
    
    Change-Id: Ic5c1ad77342c045253cfd093c76706862ed6fd0e

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -75,6 +75,11 @@ ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
     QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
 
+# Add dataservices to PRODUCT_SOONG_NAMESPACES if needed
+ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
+    PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/dataservices
+endif
+
 # Add display-commonsys-intf to PRODUCT_SOONG_NAMESPACES for QSSI supported platforms
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
     PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys-intf/display
