a0e9777769c1ab0d79e60e277a4089c266db043b
TheScarastic <warabhishek@gmail.com>
2021-10-12 06:09:57 +0000

    soong: Add udfps

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_mikuGlobalVars += \
     additional_gralloc_10_usage_bits \
     supports_extended_compress_format \
     target_init_vendor_lib \
+    target_surfaceflinger_udfps_lib \
     uses_qti_camera_device
 
 SOONG_CONFIG_NAMESPACES += mikuQcomVars
@@ -50,10 +51,12 @@ SOONG_CONFIG_mikuQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_mikuGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_mikuGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_mikuGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_mikuGlobalVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
