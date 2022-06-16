f8fd5aaf5eec51da72fa65e0d6bcdaf8c0202848
Alexander Koskovich <akoskovich@pm.me>
2022-06-16 13:14:14 -0700

    kernel: Move TARGET_KERNEL_ADDITIONAL_FLAGS to kernel task
     * Currently, because DTC_EXT make flag is in the
       kernel task the only way to override it is to
       have TARGET_KERNEL_ADDITIONAL_FLAGS come after.
    
    Change-Id: If62ed979581dc07eafd4628ce2b2d799388d3704

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -21,6 +21,7 @@
 #
 # These config vars are usually set in BoardConfig.mk:
 #
+#   TARGET_KERNEL_ADDITIONAL_FLAGS     = Additional make flags, optional
 #   TARGET_KERNEL_CONFIG               = Kernel defconfig
 #   TARGET_KERNEL_VARIANT_CONFIG       = Variant defconfig, optional
 #   TARGET_KERNEL_SELINUX_CONFIG       = SELinux defconfig, optional
@@ -243,6 +244,10 @@ PATH_OVERRIDE += PATH=$(KERNEL_TOOLCHAIN_PATH_gcc)/bin:$$PATH
 PATH_OVERRIDE += $(TOOLS_PATH_OVERRIDE)
 
 KERNEL_ADDITIONAL_CONFIG_OUT := $(KERNEL_OUT)/.additional_config
+
+ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
+    KERNEL_MAKE_FLAGS += $(TARGET_KERNEL_ADDITIONAL_FLAGS)
+endif
 
 # Internal implementation of make-kernel-target
 # $(1): output path (The value passed to O=)
