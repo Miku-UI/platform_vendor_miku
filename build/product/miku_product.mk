3a123c91b5550f7c397da8f97b41495a9d0e2611
Nolen Johnson <johnsonnolen@gmail.com>
2023-01-23 10:47:33 +0000

    miku: config: Exclude Launcher3*/Calendar packages from enforcement
    * These are built by AOSP common includes, and we can track most of
      these apps, but MikuHome is only partial rebrands of their
      source-application, which means there are conflicts when tracking
      them, so exclude them.
    
    Change-Id: I1fb893e6f5df693ddb551fa22adde9d05a60d1d8

--- build/product/miku_product.mk
+++ build/product/miku_product.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/miku/config/versioning.mk)
 $(call inherit-product, vendor/miku/config/overlay.mk)
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(MIKU_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(MIKU_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo)
 
 # Applications
 PRODUCT_PACKAGES += \
