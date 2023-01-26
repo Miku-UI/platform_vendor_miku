7b4327f96f385eaa55476a9c4358b41304735cbe
AmeChanRain <downloadbot007@gmail.com>
2023-01-26 18:35:30 +0000

    [SQUASHED]miku: Soong cleanup
    * Drop deprecated soong variables
    * surfaceflinger_qcom_extensions should be qcom var
    * Cleanup soong generator

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2021-2023 Miku UI
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
 
 # Add variables that we wish to make available to soong here.
@@ -29,51 +35,42 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += mikuGlobalVars
 SOONG_CONFIG_mikuGlobalVars += \
     additional_gralloc_10_usage_bits \
-    has_memfd_backport \
+    camera_needs_client_info_lib \
     gralloc_handle_has_reserved_size \
-    needs_camera_boottime \
     target_init_vendor_lib \
-    target_surfaceflinger_udfps_lib \
-    uses_egl_display_array
-
-SOONG_CONFIG_NAMESPACES += mikuNvidiaVars
-SOONG_CONFIG_mikuNvidiaVars += \
-    uses_nvidia_enhancements
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 SOONG_CONFIG_NAMESPACES += mikuQcomVars
 SOONG_CONFIG_mikuQcomVars += \
-    legacy_hw_disk_encryption \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_mikuGlobalVars += \
+SOONG_CONFIG_mikuQcomVars += \
     qcom_display_headers_namespace
 endif
 
 # Soong bool variables
 SOONG_CONFIG_mikuGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
-SOONG_CONFIG_mikuGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_mikuGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_mikuQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
-SOONG_CONFIG_mikuGlobalVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
-SOONG_CONFIG_mikuGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
-SOONG_CONFIG_mikuNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_mikuQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_mikuQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_mikuGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-SOONG_CONFIG_mikuGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_mikuGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_mikuGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_mikuGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_mikuGlobalVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
+SOONG_CONFIG_mikuQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
-SOONG_CONFIG_mikuGlobalVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
+SOONG_CONFIG_mikuQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
