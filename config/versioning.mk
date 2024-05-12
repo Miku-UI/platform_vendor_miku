9776d79b2632646c416a15f17db877c1b18bf762
AmeChanRain <79135062+AmeChanRain@users.noreply.github.com>
2024-05-12 22:04:31 +0800

    Improve logic for Chromium package.

--- config/versioning.mk
+++ config/versioning.mk
@@ -27,10 +27,8 @@ TARGET_PRODUCT_SHORT := $(subst miku_,,$(TARGET_DEVICE))
 ifeq ($(MIKU_GAPPS), true)
     $(call inherit-product, vendor/gms/gms.mk)
     MIKU_TYPE = GAPPS-
-else ifeq ($(MIKU_GAPPS), false)
-        ifeq ($(TARGET_WITH_PREBUILT_CHROMIUM), true)
-            PRODUCT_PACKAGES += Chromium
-        endif
+else ifeq ($(TARGET_WITH_PREBUILT_CHROMIUM), true)
+    PRODUCT_PACKAGES += Chromium
 endif
 
 ifeq ($(TARGET_MIKU_BUILD_VARIANT),OFFICIAL)
