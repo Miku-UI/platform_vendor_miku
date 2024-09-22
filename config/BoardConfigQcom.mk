49f1c865e350e00205e1de1893f2d1d24bacf4a1
Michael Bestas <mkbestas@lineageos.org>
2024-09-22 16:01:16 +0300

    qcom: Add soong config for thermal HAL
    Change-Id: I362efc809e7afbae38185431248ad70850904840

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -30,6 +30,11 @@ endif
 
 BOARD_USES_ADRENO := true
 
+# Disable thermal HAL netlink framework on UM platforms that do not support it
+ifneq ($(filter $(LEGACY_UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+    $(call soong_config_set,qti_thermal,netlink,false)
+endif
+
 # Add qtidisplay to soong config namespaces
 SOONG_CONFIG_NAMESPACES += qtidisplay
 
