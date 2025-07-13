ae5ca515dac47850e65d4e6f8549890340dc3276
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-13 18:12:41 +0000

    miku: target/product: Add GSI lunch targets
    Change-Id: Iac4ee3b8f7f3ce818550f38c1c64f79552a8001f

---
+++ build/target/product/miku_gsi_phone_x86_64.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2025 Miku UI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include device/generic/common/gsi_x86_64.mk
+include vendor/miku/build/target/product/miku_gsi_phone_common.mk
+
+PRODUCT_NAME := miku_gsi_phone_x86_64
+PRODUCT_MODEL := Phone GSI on x86_64
---
+++ build/target/product/miku_gsi_tab_common.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2025 Miku UI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, vendor/miku/build/product/miku_product_tab.mk)
+
+include vendor/miku/build/target/product/miku_gsi_common.mk
