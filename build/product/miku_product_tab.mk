1bdbf01e58b31cf52a015c41394c6fe20f966db0
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 00:41:59 +0800

    miku: product_tab: Add `android.software.freeform_window_management` permission
    * desktop windowing mode depends on it
    
    Change-Id: Iffdab6b268a3a3ac0fb78027c4900a3fef1eeac5

--- build/product/miku_product_tab.mk
+++ build/product/miku_product_tab.mk
@@ -19,6 +19,10 @@ include vendor/miku/build/product/miku_product_common.mk
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     MikuTabletFrameworksOverlay \
