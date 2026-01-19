efaac4bfc53eb11ae92a013ee1e136ac413750a0
AmeChanRain <downloadbot007@gmail.com>
2026-01-19 18:38:28 +0800

    config: Add qcwcn to qcom wlan namespace
    Change-Id: Ibb01900002f888ac999b00f04ee22229ffe7902c

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -196,4 +196,6 @@ ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
 endif
 
 # Add wlan to PRODUCT_SOONG_NAMESPACES
-PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/wlan
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/wlan \
+    hardware/qcom-caf/wlan/qcwcn
