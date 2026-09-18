0c3d826dec011ea95dd95093d3aa51dd4fa35895
LNC <79135062+AmeChanRain@users.noreply.github.com>
2026-09-18 11:30:44 +0800

    config: Exclude prebuilts/misc/protobuf_vendorcompat
    AOSP CP2A replaced libprotobuf-cpp-full-3.9.1-vendorcompat with
    21.12. Skip protobuf_vendorcompat, kernel/platform, and honor
    vendor/*/exclude-bp.mk.
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: Idc955f9f0c14a90d7cf6f2a566d6df4dd6dec621

--- build/product/miku_product_common.mk
+++ build/product/miku_product_common.mk
@@ -18,6 +18,13 @@
 # Inherit from custom extra
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
+# Exclude repos from bp scanning
+PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
+
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 # Inherit from our versioning
 $(call inherit-product, vendor/miku/config/versioning.mk)
 
